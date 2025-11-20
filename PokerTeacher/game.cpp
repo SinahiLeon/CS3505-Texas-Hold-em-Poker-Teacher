b26d5340743e02f1ce9bc3ca43e27424c8499fd1
TheIconoclaster <157529389+TheIconoclaster@users.noreply.github.com>
2025-11-19 21:57:03 -0700

    Implemented methods to update pot, player chips, and phase in Game (model)

--- PokerTeacher/game.cpp
+++ PokerTeacher/game.cpp
@@ -7,11 +7,15 @@ Game::Game(QObject *parent)
     smallBlind(5),
     bigBlind(10),
     phase(Phases::Preflop)
-{
+{}
+
+void Game::newGame() {
     players.resize(3);
     players[0].chips = 50;  // You
     players[1].chips = 50;  // Opponent 1
     players[2].chips = 50;  // Opponent 2
+
+    phase = Phases::Preflop;
 
     // Start UI with accurate values
     for (int i = 0; i < players.size(); i++) {
@@ -19,6 +23,22 @@ Game::Game(QObject *parent)
     }
     emit potUpdated(pot);
 }
+
+void Game::makeBet(int playerIndex, int chipAmount) {
+    if (playerIndex < 0 || playerIndex < players.size()) {
+        return;
+    }
+
+    getPlayer(playerIndex).chips -= chipAmount;
+    pot += chipAmount;
+    emit chipsUpdated(playerIndex, players[playerIndex].chips);
+    emit potUpdated(pot);
+}
+
+//call()
+//raise()
+
+
 
 // Gives pot to the winner (used inside fold or showdown)
 void Game::awardPotToPlayer(int playerIndex) {
@@ -32,3 +52,15 @@ void Game::awardPotToPlayer(int playerIndex) {
     pot = 0;
     emit potUpdated(pot);
 }
+
+void Game::nextPhase(){
+    if(phaseIndex == 4){
+        phaseIndex = 0;
+        phase = phaseIndices[phaseIndex];
+    }
+    else{
+        phaseIndex++;
+        phase = phaseIndices[phaseIndex];
+    }
+}
+
--- PokerTeacher/game.h
+++ PokerTeacher/game.h
@@ -5,7 +5,7 @@
 #include <QObject>
 #include <stack>
 #include <vector>
-
+#include <Qmap>
 
 class Game : public QObject
 {
@@ -17,7 +17,7 @@ public:
     enum class Phases {
         Preflop,
         Flop,   // Three community cards are dealt in the middle of the table.
-        Turn,   // Fourth card card dealt.
+        Turn ,   // Fourth card card dealt.
         River,  // The fifth and final community card is dealt.
         Showdown,   // If more than one player remains after the last betting round, players
                     // reveal their hands. The player with the best five-card hand wins the pot.
@@ -32,18 +32,20 @@ public:
         std::vector<Card*> hand;
     };
 
-
+    void newGame();
 
     Player& getPlayer(int index) { return players[index]; }
     int getPot() const { return pot; }
+    void makeBet(int playerIndex, int chipAmount);
     Phases getPhase() const { return phase; }
     void awardPotToPlayer(int playerIndex);
-
+    void nextPhase();
 
 
 signals:    // Since a value changed, signal to update UI
     void chipsUpdated(int playerIndex, int newAmount);
     void potUpdated(int newAmount);
+    //phaseUpdated
 
 private:
     // Cards
@@ -55,6 +57,8 @@ private:
     int currentBet;
     int bigBlind;
     int smallBlind;
+    int phaseIndex;
+    QMap<int, Phases> phaseIndices{{0, Phases::Preflop}, {1, Phases::Flop}, {2, Phases::Turn}, {3, Phases::River}, {4, Phases::Showdown}};
 
     Phases phase;
 };
