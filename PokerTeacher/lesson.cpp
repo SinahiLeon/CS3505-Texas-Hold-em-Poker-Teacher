20480f8a6d732761bcab8de63b7e958d175e0809
Gavin <gavin.grotegut@gmail.com>
2025-11-30 18:14:45 -0700

    Lesson implementation, other than reading in CPUs

--- PokerTeacher/lesson.cpp
+++ PokerTeacher/lesson.cpp
@@ -1,3 +1,109 @@
 #include "lesson.h"
+#include <QRegularExpression>
+using std::bad_cast;
+using std::out_of_range;
+using std::nullopt;
 
-Lesson::Lesson(QString folderPath, QObject *parent) {}
+Lesson::Lesson(QString folderPath, QObject *parent): folder(QDir(folderPath)) {
+    readFolderName();
+    findLessonFiles();
+}
+
+Lesson::Lesson(const Lesson& other)
+    : folder(other.folder)
+    , name(other.name)
+    , lessonPages(other.lessonPages)
+    , cpus(other.cpus)
+    , lessonNum(other.lessonNum)
+{
+    currentPage = lessonPages[0];
+    pageIndex = 0;
+}
+
+bool Lesson::nextPage() {
+    if (pageIndex >= lessonPages.size() - 1) {
+        return false;
+    }
+
+    pageIndex++;
+    currentPage = lessonPages[pageIndex];
+    return true;
+}
+
+optional<Lesson> Lesson::getNextLesson() {
+    int nextIndex = lessonNum + 1;
+    QDir parent = folder;
+    // This cannot return false in this context as this method is always run in a subfolder of Lessons.
+    parent.cdUp();
+
+    optional<QString> nextPath = findNextLesson(parent, nextIndex);
+
+    if (nextPath.has_value()) {
+        return optional<Lesson>(Lesson(*nextPath));
+    }
+
+    return nullopt;
+}
+
+void Lesson::readFolderName() {
+    QString folderName = folder.dirName();
+    QStringList nameParts = folderName.split("-");
+
+    if (nameParts.size() < 2) {
+        throw out_of_range("Lesson folder name " + folderName.toStdString() + " is not valid.");
+    }
+
+    bool isNum;
+    int num = nameParts[0].toInt(&isNum);
+
+    if (!isNum) {
+        // bad_cast exceptions do not include messages.
+        throw bad_cast();
+    }
+
+    lessonNum = num;
+    name = nameParts[1];
+}
+
+void Lesson::findLessonFiles() {
+    // Gets files and sorts them by name.
+    QStringList allFiles = folder.entryList(QDir::Files, QDir::Name);
+
+    // Using const here detaches allFiles.
+    for (auto& file : allFiles) {
+        if (isHtmlFile(file)) {
+            lessonPages.push_back(file);
+        }
+    }
+}
+
+bool Lesson::isHtmlFile(QString filename) {
+    QStringList fileParts = filename.split(".");
+
+    if (fileParts.size() > 1 && fileParts[fileParts.size() - 1] == "html") {
+        return true;
+    }
+
+    return false;
+}
+
+optional<QString> Lesson::findNextLesson(QDir parentDir, int index) {
+    QRegularExpression previousPath(".*/");
+    QStringList allFolders = parentDir.entryList(QDir::Dirs | QDir::NoDotAndDotDot);
+
+    for (auto& folderPath : allFolders) {
+        QString relativePath = folderPath.remove(previousPath);
+        QStringList nameParts = folderPath.split("-");
+
+        if (nameParts.size() > 0) {
+            bool isNum;
+            int num = nameParts[0].toInt(&isNum);
+
+            if (isNum && num == index) {
+                return folderPath;
+            }
+        }
+    }
+
+    return nullopt;
+}
--- PokerTeacher/lesson.h
+++ PokerTeacher/lesson.h
@@ -2,8 +2,11 @@
 #define LESSON_H
 
 #include <QObject>
+#include <QDir>
 #include "computerplayer.h"
 using std::vector;
+using std::optional;
+using std::pair;
 
 /// @brief The model for the infobox and an encapsulation of a game lesson.
 class Lesson : public QObject
@@ -12,13 +15,27 @@ class Lesson : public QObject
 
 public:
     explicit Lesson(QString folderPath, QObject *parent = nullptr);
-    string getName() { return name; };
-    vector<QString>& getLessonFiles() { return lessonFiles; };
-    vector<ComputerPlayer> getComputerPlayers() { return cpus; };
+    explicit Lesson(const Lesson& other);
+    QString getName() const { return name; };
+    QString getCurrentPage() const { return currentPage; };
+    vector<QString>& getLessonPages() { return lessonPages; };
+    vector<ComputerPlayer>& getComputerPlayers() { return cpus; };
+    int getLessonNum() const { return lessonNum; };
+    bool hasComupterPlayers() const { return !cpus.empty(); };
+    bool nextPage();
+    optional<Lesson> getNextLesson();
 private:
-    string name;
-    vector<QString> lessonFiles;
+    const QDir folder;
+    QString name;
+    QString currentPage;
+    vector<QString> lessonPages;
     vector<ComputerPlayer> cpus;
-};
+    int lessonNum;
+    int pageIndex;
 
+    void readFolderName();
+    void findLessonFiles();
+    static bool isHtmlFile(QString filename);
+    static optional<QString> findNextLesson(QDir parentDir, int index);
+};
 #endif // LESSON_H
