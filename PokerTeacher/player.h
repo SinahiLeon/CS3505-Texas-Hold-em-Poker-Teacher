0b5e4e31810f967f76fb6158367a2f878f7bfef2
Ben Foutz <bfoutz77@gmail.com>
2025-12-02 20:37:04 -0700

    make player.h match player.cpp, oops

--- PokerTeacher/player.h
+++ PokerTeacher/player.h
@@ -2,20 +2,30 @@
 #define PLAYER_H
 
 #include "cardhand.h"
+#include "action.h"
+#include <queue>
+
+using std::queue;
 
 /// @brief A player that containts their information on chips, currentBet, folded, and hand data.
 class Player {
 public:
-    Player() : fullhand(heldCards) {}
+    Player();
+    Player(bool isHuman);
+    bool const isHuman;
     bool canBet(int amount) { return chips >= amount; }
     int chips = 1000;
     int currentBet = 0;
     bool folded = false;
     bool handVisible = false;
     vector<shared_ptr<Card>> heldCards;
+    queue<Action> decisions;
 
     // Combined community and personal hand - will reference shared_ptrs
     CardHand fullhand;
+
+    Action makeDecision();
+
 };
 
 #endif // PLAYER_H
